6b5c3dbfaa09e1d1ad7f1d7722952e6f286633e9
Ashutosh Sharma <ashutosh.ssv@gmail.com>
2022-08-18 18:44:52 +0530

    Personal Website 2
    Minimal Design

---
+++ js/custom.js
@@ -0,0 +1,211 @@
+(function($) { 
+		  
+$(document).ready(function(){
+	$('.slide').prepend('<div class="patternOverlay"></div>');	
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},6000);
+	});
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skill-bar-percent').animate({
+			left:$(this).attr('data-percent')
+		},6000);
+	});
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skill-bar-percent').text($(this).attr('data-percent'));
+	});
+	
+	if($(window).width()>767){
+		$('.navbar-wrapper').animate({
+			opacity:0
+		},200);
+		$('.navbar-wrapper').addClass('default-menu');
+		
+	}	
+	
+	$('#nav-show').click(function(x){
+		x.preventDefault();
+		$('.wrapper').removeClass('noGap');
+		$('.navbar-wrapper').animate({
+			opacity:1
+		},200);
+		$('.navbar-wrapper').removeClass('default-menu');
+		$(this).fadeOut();
+	});
+	
+	$('.closeMenu').click(function(x){
+		x.preventDefault();
+		$('.navbar-wrapper').animate({
+			opacity:0
+		},50,function(){$('.navbar-wrapper').addClass('default-menu');});
+		
+		$('#nav-show').fadeIn();
+	});
+	
+	
+});	
+
+
+	
+/*===========================================================*/
+	/*	Preloader 
+/*===========================================================*/	
+//<![CDATA[
+	$(window).load(function() { // makes sure the whole site is loaded
+		$("#status").fadeOut(); // will first fade out the loading animation
+		$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+	})
+//]]>
+		  
+// for mobile nav js	
+$(window).load(function(){
+	$('button.navbar-toggle').click(function(){
+		$(this).toggleClass('active');
+		$('.navbar-collapse').slideToggle();										 
+	});					
+});
+// for banner height js
+var windowWidth = $(window).width();
+var windowHeight =$(window).height();
+$('.banner').css({'height':windowHeight });
+
+
+// for portfoli filter jquary
+$(window).load(function(){
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
+
+
+
+// for portfoli lightbox jquary
+jQuery(function() {
+	var $chosenSheet,
+	$stylesheets = $( "a[id^=theme-]" );
+	
+	// run rlightbox
+	$( ".lb" ).rlightbox();
+	$( ".lb_title-overwritten" ).rlightbox({overwriteTitle: true});
+});
+});
+
+// Somth page scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -16
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// Testimonial slider
+$(window).load(function(){
+  $('.flexslider').flexslider({
+	animation: "fade",
+	animationLoop: true,
+	slideshow: true,
+	pauseOnAction: false,
+	slideshowSpeed: 7000,
+	controlNav: true,
+	start: function(slider){
+	  $('body').removeClass('loading');
+	}
+  });
+});
+
+
+// we worked slider
+$(window).load(function(){
+      $('.worklogo').flexslider({
+        animation: "slide",
+		slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        
+      });
+    });
+
+// Skill set 
+
+
+var span = document.querySelector(".typewriter span");
+var textArr = span.getAttribute("data-text").split(", ");
+var maxTextIndex = textArr.length;
+
+var sPerChar = 0.15;
+var sBetweenWord = 1.5;
+var textIndex = 0;
+
+typing(textIndex, textArr[textIndex]);
+
+function typing(textIndex, text) {
+  var charIndex = 0;
+  var maxCharIndex = text.length - 1;
+
+  var typeInterval = setInterval(function () {
+    span.innerHTML += text[charIndex];
+    if (charIndex == maxCharIndex) {
+      clearInterval(typeInterval);
+      setTimeout(function () {
+        deleting(textIndex, text);
+      }, sBetweenWord * 1000);
+    } else {
+      charIndex += 1;
+    }
+  }, sPerChar * 200);
+}
+
+function deleting(textIndex, text) {
+  var minCharIndex = 0;
+  var charIndex = text.length - 1;
+
+  var typeInterval = setInterval(function () {
+    span.innerHTML = text.substr(0, charIndex);
+    if (charIndex == minCharIndex) {
+      clearInterval(typeInterval);
+      textIndex + 1 == maxTextIndex ? (textIndex = 0) : (textIndex += 1);
+      setTimeout(function () {
+        typing(textIndex, textArr[textIndex]);
+      }, sBetweenWord * 200);
+    } else {
+      charIndex -= 1;
+    }
+  }, sPerChar * 200);
+}
+
+
+}(jQuery));
