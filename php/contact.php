eed922ea2db477b29c1b44f6ca404c692a1fcf90
Ashutosh Sharma <ashutosh.ssv@gmail.com>
2022-08-19 16:03:25 +0530

    n

--- php/contact.php
+++ php/contact.php
@@ -29,11 +29,6 @@ if(trim($comments) == '') {
 	exit();
 }
 
-if(get_magic_quotes_gpc()) {
-	$comments = stripslashes($comments);
-}
-
-
 // Configuration option.
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
